ca8834a5d8bfe2a51d0f1d563ca36037021dfbbb
Eugene-TechnoNinja <technoninja@ukr.net>
2025-10-01 10:42:07 +0300

    added all constants

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -6,12 +6,9 @@ using namespace std;
 
 enum { SIZE_BIT, MAX_NUM };
 const char *notification = " bit unsigned integer maximum is ";
-const unsigned short uCharValue[] = {sizeof(char) * CHAR_BIT,
-                                     (unsigned short)(pow(2, CHAR_BIT) - 1)};
-const unsigned int uShortValue[] = {sizeof(short) * CHAR_BIT,
-                                    (unsigned int)(pow(2, CHAR_BIT * 2) - 1)};
-const unsigned int uIntValue[] = {sizeof(int) * CHAR_BIT,
-                                  (unsigned int)pow(2, CHAR_BIT * 4) - 1};
+const unsigned short uCharValue[] = {sizeof(char) * CHAR_BIT, UCHAR_MAX};
+const unsigned int uShortValue[] = {sizeof(short) * CHAR_BIT, USHRT_MAX};
+const unsigned int uIntValue[] = {sizeof(int) * CHAR_BIT, UINT_MAX};
 const unsigned long long uLLValue[] = {sizeof(long long) * CHAR_BIT,
                                        ULLONG_MAX};
 

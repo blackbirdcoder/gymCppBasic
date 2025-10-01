400096e929a3ad0fa43bec02974021a32d90595c
Eugene-TechnoNinja <technoninja@ukr.net>
2025-10-01 10:33:28 +0300

    added constant ull

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -12,15 +12,14 @@ const unsigned int uShortValue[] = {sizeof(short) * CHAR_BIT,
                                     (unsigned int)(pow(2, CHAR_BIT * 2) - 1)};
 const unsigned int uIntValue[] = {sizeof(int) * CHAR_BIT,
                                   (unsigned int)pow(2, CHAR_BIT * 4) - 1};
-const unsigned long long uLLValue[] = {
-    sizeof(long long) * CHAR_BIT, (unsigned long long)pow(2, CHAR_BIT * 8)};
+const unsigned long long uLLValue[] = {sizeof(long long) * CHAR_BIT,
+                                       ULLONG_MAX};
 
 int main(void) {
   cout << uCharValue[SIZE_BIT] << notification << uCharValue[MAX_NUM] << "\n";
   cout << uShortValue[SIZE_BIT] << notification << uShortValue[MAX_NUM] << "\n";
   cout << uIntValue[SIZE_BIT] << notification << uIntValue[MAX_NUM] << "\n";
-  cout << uLLValue[SIZE_BIT] << notification
-       << uLLValue[MAX_NUM] + uLLValue[MAX_NUM] - 1 << "\n";
+  cout << uLLValue[SIZE_BIT] << notification << uLLValue[MAX_NUM] << "\n";
 
   return 0;
 }
